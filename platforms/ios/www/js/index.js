86e26a3aeb75035403b60a542dce2458d010b921
Elab0009 <elab0009@algonquinlive.com>
2017-04-15 10:46:37 -0400

    My frist Upload

---
+++ platforms/ios/www/js/index.js
@@ -0,0 +1,207 @@
+//Ahamad Elabd
+//2017-04-10
+var app = {
+        rating: 0
+        , image: ""
+        , setRating: function (stars) {
+        [].forEach.call(stars, function (star, index) {
+                if (app.rating > index) {
+                    star.classList.add('rated');
+                }
+                else {
+                    star.classList.remove('rated');
+                }
+            });
+        }
+        , save: function () {
+            let name = document.getElementById("itemName").value;
+            if (name == "" || app.rating == 0 || app.image == "") {
+                alert("fill  all");
+            }
+            else {
+                let data = JSON.parse(localStorage.getItem("reviewR-elab0009"));
+                if (data == null) {
+                    //create array of people
+                    data = [];
+                }
+                data.push({
+                    "id": Date.now()
+                    , "name": name
+                    , "rating": app.rating
+                    , "image" :app.image
+                });
+                localStorage.setItem("reviewR-elab0009", JSON.stringify(data));
+                app.showList();
+                app.closeModal();
+               
+            }
+        }
+        , cleanModal: function () {
+            app.rating = 0;
+            document.getElementById("itemName").value = '';
+            app.setRating(document.querySelectorAll("#Addmodal .star"));
+            app.image = "";
+            document.getElementById("img").classList.add("hidden");
+            document.getElementById("takeImg").classList.remove("hidden");
+        }
+        , closeModal: function () {
+            app.cleanModal();
+            let event = new CustomEvent("touchend", {
+                bubbles: true
+                , cancelable: true
+            });
+            document.getElementById("closebutton").dispatchEvent(event);
+        }
+        ,takePic: function(){
+         let options = {
+                            quality: 80,
+                            destinationType: Camera.DestinationType.FILE_URI,
+                            encodingType: Camera.EncodingType.PNG,
+                            mediaType: Camera.MediaType.PICTURE,
+                            pictureSourceType: Camera.PictureSourceType.CAMERA,
+                            allowEdit: true,
+                            targetWidth: 300,
+                            targetHeight: 300
+         };
+        
+         
+         navigator.camera.getPicture( app.successCallback, app.err, options );   
+        },
+            
+        successCallback: function(imgData){
+            app.image = imgData;
+            document.getElementById("img").src = imgData;
+            document.getElementById("img").classList.remove("hidden");
+            document.getElementById("takeImg").classList.add("hidden");
+            
+        },
+    
+        err : function( error ){
+            console.log("error");
+            alert(error);
+        },
+       
+    
+        showList: function () {
+            let data = JSON.parse(localStorage.getItem("reviewR-elab0009"));
+            if (data != null) {
+                let E = document.getElementById("Reviews");
+                E.innerHTML = "";
+                for (let i = 0; i < data.length; i++) {
+                    let listItem = document.createElement("li");
+                    listItem.className = "table-view-cell";
+                    let a = document.createElement("a");
+                    a.className = "navigate-right";
+                    a.setAttribute("href","#rmvmodal");
+                    let img = document.createElement("img");
+                    img.className = "media-object pull-left";
+                    if(data[i].image == ""){
+                        img.src = "img/placeholder.jpg";
+                    } else {
+                        img.src = data[i].image;   
+                    }
+                    let divMediaBody = document.createElement("div");
+                    divMediaBody.className = "media-body";
+                    let bodySpan = document.createElement("span");
+                    bodySpan.className = "name";
+                    bodySpan.textContent = data[i].name;
+                    let reveiewStars = document.createElement("div");
+                    reveiewStars.className = "stars review-list";
+                    let starA = document.createElement("span");
+                    starA.className = "star";
+                    let starB = document.createElement("span");
+                    starB.className = "star";
+                    let starC = document.createElement("span");
+                    starC.className = "star";
+                    let starD = document.createElement("span");
+                    starD.className = "star";
+                    let starF = document.createElement("span");
+                    starF.className = "star";
+                    
+                    listItem.appendChild(a);
+                    a.appendChild(img);
+                    a.appendChild(divMediaBody);
+                    divMediaBody.appendChild(bodySpan);
+                    divMediaBody.appendChild(reveiewStars);
+                    reveiewStars.appendChild(starA);
+                    reveiewStars.appendChild(starB);
+                    reveiewStars.appendChild(starC);
+                    reveiewStars.appendChild(starD);
+                    reveiewStars.appendChild(starF);
+                    
+                    app.rating = data[i].rating;
+                    app.setRating(reveiewStars.querySelectorAll(".star"));
+                    a.addEventListener("touchend", function(){app.openRmvmodal(data[i].id)});
+                    
+                    E.appendChild(listItem);
+                }
+            }
+        },
+        openRmvmodal : function(id){
+            let data = JSON.parse(localStorage.getItem("reviewR-elab0009"));
+            
+             for(let i = 0; i < data.length; i++){ 
+            if(id == data[i].id){
+                
+                document.getElementById("rmvname").textContent = data[i].name;
+                app.rating = data[i].rating;
+                app.setRating(document.querySelectorAll("#rmvmodal .star"));
+                
+                if(data[i].image == "" ){
+                    document.getElementById("pic").src = "img/placeholder.jpg";
+                    
+                }else{
+                document.getElementById("pic").src = data[i].image;
+                }
+                document.getElementById("deleteItem").addEventListener("click", function(){app.removeItem(id)});
+            }
+       }
+    },
+    
+    removeItem : function(id){
+    let data = JSON.parse(localStorage.getItem("reviewR-elab0009"));
+        for(let i = 0; i < data.length ; i++){
+             if(data[i].id == id ){
+             
+                data.splice(i,1);
+                 
+                localStorage.setItem("reviewR-elab0009",JSON.stringify(data));
+                
+                 app.showList();  
+                 let event = new CustomEvent("touchend", {
+                    bubbles: true
+                    , cancelable: true
+                });
+                document.getElementById("closebuttonM").dispatchEvent(event);
+                break;
+             }
+        }
+        
+        
+        app.showList();
+    },      
+            
+        
+     init: function () {
+            document.addEventListener('deviceready', app.onDeviceReady, false);
+        }
+        , onDeviceReady: function () {
+            app.showList();
+            let stars = document.querySelectorAll("#Addmodal .star");
+            //console.log(stars);
+        [].forEach.call(stars, function (star, index) {
+                star.addEventListener('click', (function (idx) {
+                    return function () {
+                        app.rating = idx + 1;
+                        app.setRating(stars);
+                    }
+                })(index));
+            });
+            document.getElementById("saveItem").addEventListener("click", app.save);
+            document.getElementById("canItem").addEventListener("click", app.closeModal);
+            document.getElementById("closebutton").addEventListener("touchend", app.cleanModal);
+            document.getElementById("takeImg").addEventListener("click", app.takePic);
+        }
+    }
+    //app.init();
+app.onDeviceReady();
